6080a8092f3533e6da0570bfe65cd35029eb7fb7
Felipe B <pipeb62@gmail.com>
2024-05-28 07:45:24 -0600

    optimizacion de test.jl

--- analysis/test.jl
+++ analysis/test.jl
@@ -1,49 +1,64 @@
 using Profile
+using StaticArrays
+using BenchmarkTools
 
-function dot(u1, u2) 
-    return sum(u1.*u2)
+function cdot(u1::SVector{3,Float64}, u2::SVector{3,Float64}) 
+    sum(u1.*u2)
 end
 
-function norm(u)
-    return sqrt(sum(u.^2))
+function norm(u::SVector{3,Float64})
+    return sqrt(cdot(u,u))
 end
 
-function test_overlap(r::Array{Float64}, rho::Float64, sample_sz::Int64, L::Float64)
-    current_distr = Vector{Float64}([])
-    l = Array{Float64,2}(undef,3,2)
-    ul = Vector{Float64}(undef,3)
-    @views for k in 1:sample_sz
-        l .= L.*rand(3,2)
-        ul .= (l[:,1]-l[:,2])./norm(l[:,1]-l[:,2])
-        overlap = false
-        d_perp::Float64 = 0.0
-        for i in 1:size(r,2)
-            d_pll2::Float64 = dot(ul, r[:,i]-l[:,2])
-            d_pll1::Float64 = dot(-ul, r[:,i]-l[:,1])
-            if ((d_pll2>0) && (d_pll1>0))        
-                d_perp = norm((r[:,i]-l[:,2]) - ul*dot(ul, r[:,i]-l[:,2])) 
-                if d_perp<rho
-                    overlap = true
-                    break
-                end
-            end
-        end
-        if overlap == false
-            push!(current_distr,d_perp)
+@views function check_overlap(r::SVector{3,Float64},l1::SVector{3,Float64},l2::SVector{3,Float64},rho::Float64)
+    ul = SVector{3,Float64}((l1-l2)/norm(l1-l2))
+    ri1 = SVector{3,Float64}(r-l1)
+    ri2 = SVector{3,Float64}(r-l2)
+    d_pll2::Float64 = cdot(ul, ri2)
+    d_pll1::Float64 = cdot(-ul, ri1)
+    overlap = false
+    if ((d_pll2>0) && (d_pll1>0))        
+        d_perp = norm(ri2 - ul*d_pll2) 
+        if d_perp<rho
+            overlap = true
         end
     end
-    return current_distr
+    return overlap
+end
+
+@views function line_distr(r::Vector{SVector{3,Float64}},sample_sz::Int64,L::Float64,rho::Float64)
+    distr = Vector{Float64}()
+    for _ in 1:sample_sz
+        l1 = L* @SVector rand(Float64,3)
+        l2 = L* @SVector rand(Float64,3)
+        overlap = true
+        for i in 1:size(r,1)
+            overlap = check_overlap(r[i],l1,l2,rho)
+            if overlap
+                break
+            end
+        end
+        if !overlap 
+            push!(distr,norm(l1-l2))
+        end
+    end
+    return distr
 end
 
 L = 54.0
-r = L.*rand(3,16000)
 rho = 2^(1/6)
-sample_sz = 10000
+sample_sz = 1000000
+r = [L*rand(SVector{3, Float64}) for i in 1:16000]
 
-test_overlap(r,rho,sample_sz,L)
+@btime line_distr($r,$sample_sz,$L,$rho)
+#@code_warntype line_distr(r,sample_sz,L,rho)
+#@profview line_distr(r,sample_sz,L,rho)
 
-@time a = test_overlap(r,rho,sample_sz,L)
-println(length(a))
+#l1 = L* @SVector rand(Float64,3)
+#l2 = L* @SVector rand(Float64,3)
 
-#@profile test_overlap(r,rho,sample_sz,L)
-#Profile.print()
+#@code_warntype check_overlap(r[1],l1,l2,rho)
+
+
+
+
