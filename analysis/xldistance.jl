eea2bf4b96cdcf7b5902081015a83e8cf0d83211
Pipe Benavides <pipeb62@gmail.com>
2024-07-15 17:41:43 -0600

    xldistance

---
+++ analysis/xldistance.jl
@@ -0,0 +1,45 @@
+function read_frame_num(dumpdir)
+    dump = open(dumpdir,"r") 
+    frame_num = 0
+    for line in readlines(dump)
+        if line == "ITEM: TIMESTEP"
+            frame_num += 1
+        end
+    end
+    close(dump)
+    return frame_num 
+end
+
+#Lee la infromacion del frame del dump
+function read_dump_info(dump)
+    readline(dump) #saltar texto ITEM: TIMESTEP
+    timestep = readline(dump) #leer iteracion actual
+    readline(dump) #saltar texto ITEM: NUMBER OF ATOMS
+    n = parse(Int64,readline(dump)) #leer numero de atomos
+    readline(dump) #saltar texto ITEM: BOX BOUNDS xy xz yz pp pp pp
+    #Leer coords de caja en x. Leer xy
+    xline = [parse(Float64,ii) for ii in split(readline(dump)," ")] 
+    xy = xline[3]
+    #Leer coords de caja en y. Leer yz
+    yline = [parse(Float64,ii) for ii in split(readline(dump)," ")]
+    L = yline[2]-yline[1]
+    readline(dump) #Leer coords de caja en z. Leer xz
+    readline(dump) #saltar texto ITEM: ATOMS id type x y z
+
+    return timestep,n,L,xy
+end
+
+#Lee las coordenads de las particulas del dump para un frame
+function read_dump_coords(dump,n::Int64)
+    r = Vector{SVector{3,Float64}}()
+    for _ in 1:n
+        line = split(readline(dump)," ")
+        if line[2] == "2" || line[2] == "4"
+            continue
+        else
+            ri = @SVector [parse(Float64,line[ii]) for ii in 3:5]
+            push!(r,ri)
+        end
+    end
+    return r
+end
